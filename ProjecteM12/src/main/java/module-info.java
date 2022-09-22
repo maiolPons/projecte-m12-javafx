88db9c822331a311b1ea609e4a18f8fb0c0059b8
maiol Pons <100287697+maiolPons@users.noreply.github.com>
2022-09-22 01:34:11 -0700

    v6

--- ProjecteM12/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.projectem12 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    opens com.mycompany.projectem12 to javafx.fxml;
-    exports com.mycompany.projectem12;
-}
